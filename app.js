23f100b2279e28e3fd067ecec37124b9d3976e7e
MeetaMermaid <ashmeeta.s.maharaj@gmail.com>
2025-04-15 18:31:32 -0700

    Exercise 3 successfully printed

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 // console.log('sanity check')
-// /*
-// Exercise 1: Define an empty array
+// // /*
+// // Exercise 1: Define an empty array
 
-// 1) Create an empty array and assign it to a variable called `foods`.
+// // 1) Create an empty array and assign it to a variable called `foods`.
 
-// Exercise 1 has been completed for you:
-// */
+// // Exercise 1 has been completed for you:
+// // */
+
+// // const foods = [];  
+
+// // console.log('Exercise 1 result:', foods);
+
+// // /*
+// // Exercise 2: Add strings to the array
+
+// // 1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+// // Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+// // Complete Exercise 2 in the space below:
+// // */
 
 // const foods = [];  
-
-// console.log('Exercise 1 result:', foods);
+// foods.push('pizza');
+// foods.push('cheeseburger');
+// console.log('Exercise 2 result:', foods);
 
 // /*
-// Exercise 2: Add strings to the array
+// Exercise 3: Insert at the beginning
 
-// 1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+// 1) Insert the string 'taco' at the beginning of the `foods` array.
 
-// Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
-
-// Complete Exercise 2 in the space below:
+// Complete Exercise 3 in the space below:
 // */
+
+// // NOTE: To add an item to the beginning, it's .unshift()
 
 const foods = [];  
 foods.push('pizza');
 foods.push('cheeseburger');
+foods.unshift('taco');
 console.log('Exercise 2 result:', foods);
+console.log('Exercise 3 result:', foods);
