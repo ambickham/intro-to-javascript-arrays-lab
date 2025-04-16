1817ebb1a194a467a5373a5bd9ab424a7bbacebf
MeetaMermaid <ashmeeta.s.maharaj@gmail.com>
2025-04-15 23:59:22 -0700

    Successfully completed 14th exercise

--- app.js
+++ app.js
@@ -252,16 +252,38 @@ Exercise 13: Retrieve the Last Array
    - Do not alter the original `numArrays` array.
 
 Complete Exercise 13 in the space below:
-*/
+// */
 
-const numArrays = [
-	[100, 5, 23],
-	[15, 21, 72, 9],
-	[45, 66],
-	[7, 81, 90]
-];
-const numList = numArrays[numArrays.length-1];
-console.log('Exercise 13 result:', numList);
+// const numArrays = [
+// 	[100, 5, 23],
+// 	[15, 21, 72, 9],
+// 	[45, 66],
+// 	[7, 81, 90]
+// ];
+// const numList = numArrays[numArrays.length-1];
+// console.log('Exercise 13 result:', numList);
 
 // Exercise 13 result: (3) [7, 81, 90]
 // Printed successfully! 
+
+/*
+Exercise 14: Accessing within nested arrays
+
+1) Retrieve the number `66` from the `numArrays` array. As part of this process
+   do not alter the original `numArrays` array.
+
+2) Assign it to a variable called `num`.
+
+Complete Exercise 14 in the space below:
+*/
+// const numArrays = [
+//     	[100, 5, 23],
+//     	[15, 21, 72, 9],
+//     	[45, 66],
+//     	[7, 81, 90]
+//     ];
+// const num = numArrays [2][1]; // dont need a comma beetween numbers. 
+// console.log('Exercise 14 result:', num);
+
+// Exercise 14 result: 66
+
